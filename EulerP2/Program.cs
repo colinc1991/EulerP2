f2becd6341010d714ae0d9e3eaa8393fd6f1978e
Colin Campbell <colincampbell1991@gmail.com>
2019-06-03 13:19:15 +0100

    Final commit.

--- EulerP2/Program.cs
+++ EulerP2/Program.cs
@@ -6,7 +6,24 @@ namespace EulerP2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numOne = 1;
+            int numTwo = 2;
+            int placeHolder = 0;
+            int sum = 0;
+
+            while (numOne < 4000000 && numTwo < 4000000)
+            {
+                if (numTwo % 2 == 0)
+                {
+                    sum += numTwo;
+                }
+
+                placeHolder = numTwo;
+                numTwo = numOne + numTwo;
+                numOne = placeHolder;
+            }
+
+            Console.WriteLine($"The sum of the even valued terms of the Fibonacci sequence whose values do not exceed four million is: {sum}");
         }
     }
 }
